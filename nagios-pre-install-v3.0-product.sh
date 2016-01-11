7bd95d9b7f8750ec1a6f799315461d844240d99d
houqian <houqian1991@foxmail.com>
2016-01-11 20:38:23 +0800

    Update nagios-pre-install-v3.0-product.sh

--- nagios-pre-install-v3.0-product.sh
+++ nagios-pre-install-v3.0-product.sh
@@ -19,7 +19,7 @@ endColor='\e[0m'
 YUM_URL="http://yum.110monitor.com"
 INSTALL_PROCESS_URL="http://api.110monitor.com/alert/api/escalation/agentInstall/$NAGIOS_APPKEY"
 MENU_CEP_URL="http://api.110monitor.com/alert/api/event"
-HB_URL="http://api.110monitor.com/alert/api/heartbeat/"
+HB_URL="http://hb.110monitor.com/alert/api/heartbeat/"
 MENU_ALERT_URL="http://api.110monitor.com/alert"
 
 # detect whether the current user is root.
